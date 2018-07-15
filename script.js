c3839647d21098c2aa4b85729a464f8c619dd526
Rahul Pugal <rahul.pugal@mountblue.io>
2018-07-15 14:10:50 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+	var menu = document.getElementById("menu");
+	menu.classList.add("show");
+}
+function hideMenu() {
+	var menu = document.getElementById("menu");
+	menu.classList.remove("show")
+}
